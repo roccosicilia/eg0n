044a8c68885500c4128d3c0365c82215aa2f96ef
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-11-12 18:24:21 +0100

    test domain enum

---
+++ runbook/domainenum.py
@@ -0,0 +1,81 @@
+# Descr: get domain name informations
+# Usage: dnsenum.py TargetDomain.url
+
+import sys, time, requests, json
+import dns.resolver, mysql.connector
+from config import *
+
+##### Base functions ##############################################################################
+def dns_query(base_url, type):
+    try:
+        query = dns.resolver.resolve(base_url, type)
+    except:
+        print("Query error.")
+    return query
+
+def dbinsert(query):
+    myconn = mysql.connector.connect(host=mydb["host"], user=mydb["user"], password=mydb["pass"], database=mydb["name"], auth_plugin='mysql_native_password')
+    mycursor = myconn.cursor()
+    mycursor.execute(query)
+
+def dbselect(query):
+    myconn = mysql.connector.connect(host=mydb["host"], user=mydb["user"], password=mydb["pass"], database=mydb["name"], auth_plugin='mysql_native_password')
+    mycursor = myconn.cursor()
+    mycursor.execute(query)
+    result = mycursor.fetchall()
+    return result
+
+
+
+if __name__ == "__main__":
+
+    ##### Local data ##############################################################################
+    base_url = sys.argv[2]
+    timestamp_corrente = int(time.time())
+
+    ##### Define TARGET list
+    query_targetlist = "SELECT * FROM `target` ORDER BY `id`"
+    targetlist = dbselect(query_targetlist)
+
+    for target in targetlist:
+        print(target)
+
+'''
+    # check A record
+    base_url = sys.argv[1]
+    r = dns_query(base_url, 'A')
+
+    # IP list for A record
+    arecord = ''
+    for data in r:
+        arecord += "{0} ".format(data)
+    arecord_list = arecord.split(" ")
+
+    # report A Record
+    dspace = 29 - len(f"{r}.{domain}")
+    print("#"*100)
+    print("| Record Type\t | Domain Name                    | IP list\t")
+    print("| {0}\t\t | {1}\t | {2}".format('A', domain, IPRecordA))
+
+    # brute force DNS name
+    bflist = [
+        "act", "admin", "app", "art", "baking", "beauty", "blog", "books", "business", "cars", "chat", 
+        "code", "cooking", "crafts", "culture", "design", "development", "education", 
+        "email", "entertainment", "environment", "events", "exchange", "fashion", "finance", "fitness", "food", 
+        "forum", "ftp", "gaming", "garden", "health", "help", "history", "home", "innovation", 
+        "investing", "jobs", "law", "literature", "mail", "marketing", "media", "members", 
+        "motivation", "music", "nature", "news", "outdoors", "passion", "pets", "philosophy", 
+        "photos", "politics", "portfolio", "realty", "recipes", "religion", "science", "shop", 
+        "social", "spirituality", "sports", "startup", "store", "style", "success", "support", 
+        "tech", "technology", "travel", "tv", "videos", "wellness", "writing", "www"
+    ]
+'''    
+'''    print("#"*100)
+    for r in bflist:
+        try:
+            thirdleveldomain = r+"."+domain
+            dspace = 29 - len(thirdleveldomain)
+            ip = socket.gethostbyname(f"{thirdleveldomain}")
+            print("| {0}\t\t | {1} {2} | {3}".format('-', f"{thirdleveldomain}", ' '*dspace, ip))
+        except:
+            print("| {0}\t\t | {1} {2} | {3}".format('-', f"{thirdleveldomain}", ' '*dspace, '-'))'''
