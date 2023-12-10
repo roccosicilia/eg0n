55c2196ae73e38fbfef58b9ebdb9c67e1b735b1d
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-12-11 00:37:05 +0100

    cwe stats

--- runbook/stats_cwe.py
+++ runbook/stats_cwe.py
@@ -8,12 +8,25 @@ def cwe_count():
     mycursor = myconn.cursor()
 
     # cwe count from DB
-    query = "SELECT cwe, COUNT(*) as count FROM cve_list GROUP BY cwe ORDER BY count DESC"
+    query = "SELECT cwe, COUNT(*) as count FROM `cve_list` GROUP BY cwe ORDER BY count DESC"
     mycursor.execute(query)
     result = mycursor.fetchall()
 
     for row in result:
-        print(row)
+        if (row[1] >= 10):
+            query = "SELECT * FROM `cwe_stats` WHERE cwe = {}".format(row[0])
+            mycursor.execute(query)
+            result = mycursor.fetchall()
+            if len(result) >= 1:
+                # update DB
+                query_update = "UPDATE `cwe_stats` SET count = {} WHERE cwe = {}".format(row[1], row[0])
+                mycursor.execute(query_update)
+                myconn.commit()
+            else:
+                # insert new cwe
+                query_insert = "INSERT INTO `cwe_stats` (cwe, count) VALUES ('{}', '{}')".format(row[0], row[1])
+                mycursor.execute(query_insert)
+                myconn.commit()
 
 if __name__ == "__main__":
 
