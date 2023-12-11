9ab4a6f0410e7e3e1587144b664379c9c013083d
Rocco Sicilia <rocco.sicilia@protonmail.com>
2023-12-11 15:47:52 +0100

    add cve_stats

--- runbook/genStats.py
+++ runbook/genStats.py
@@ -30,6 +30,36 @@ def cwe_count():
                 myconn.commit()
                 print("NEW cwe record!")
 
+def cve_count():
+    # db connect
+    myconn = mysql.connector.connect(host=mydb["host"], user=mydb["user"], password=mydb["pass"], database=mydb["name"], auth_plugin='mysql_native_password')
+    mycursor = myconn.cursor()
+
+    # cve count from DB
+    query = "SELECT date_published,COUNT(*) as count FROM cve_list GROUP BY date_published ORDER BY date_published DESC"
+    mycursor.execute(query)
+    result = mycursor.fetchall()
+
+    for row in result:
+        if (row[1] >= 10):
+            query = "SELECT * FROM `cve_stats` WHERE date_published = '{}'".format(row[0])
+            mycursor.execute(query)
+            result = mycursor.fetchall()
+            if len(result) >= 1:
+                # update DB
+                query_update = "UPDATE `cve_stats` SET cve_count = '{}' WHERE cwe = '{}'".format(row[1], row[0])
+                mycursor.execute(query_update)
+                myconn.commit()
+                print("Update cwe records.")
+            else:
+                # insert daily data
+                query_insert = "INSERT INTO `cve_stats` (date_published, cve_count) VALUES ('{}', '{}')".format(row[0], row[1])
+                mycursor.execute(query_insert)
+                myconn.commit()
+                print("NEW cwe record!")
+
 if __name__ == "__main__":
 
     cwe_count()
+    cve_count()
+    
