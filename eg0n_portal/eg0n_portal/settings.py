579dc97ac5a0971b0c2bf1ca4e1b177831c565de
Rocco Sicilia <rocco.sicilia@protonmail.com>
2024-12-15 11:52:58 +0100

    no dashboard

--- eg0n_portal/eg0n_portal/settings.py
+++ eg0n_portal/eg0n_portal/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'infosec_dashboard',
     'ioc_management',
     'info_gathering',
 
